d539b4bdc59c4f7feebf51d2bdb361bf4e4fa963
kingwmk <1171147450@qq.com>
2021-07-05 10:36:32 +0800

    Update lstm_train_test.py

--- lstm_train_test.py
+++ lstm_train_test.py
@@ -121,7 +121,7 @@ def parse_arguments() -> Any:
                         help="Val batch size")
     parser.add_argument("--end_epoch",
                         type=int,
-                        default=500,
+                        default=100,
                         help="Last epoch")
     parser.add_argument("--lr",
                         type=float,
